1a656d8140a7840532086779b39bbccda2e69fd6
Conor Donohoe <conordonohoe1234567@gmail.com>
2018-12-19 16:47:51 +0000

    #11 - Added error toasters

--- www/src/templates/results/results.controller.js
+++ www/src/templates/results/results.controller.js
@@ -1,4 +1,4 @@
-export default function homeController($scope, $routeParams, $http, $location, $interval, $localStorage) {
+export default function homeController($scope, $routeParams, $http, $location, $interval, $localStorage, $mdToast) {
 	$scope.stops = [];
 	$scope.stop = [];
 	$scope.isFavourite = false;
@@ -20,51 +20,56 @@ export default function homeController($scope, $routeParams, $http, $location, $
 	$scope.url = "https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=" + $routeParams.stopid + "&format=json";
 	$scope.checkStop = function(){
 		$http.get($scope.url).then(function (response) {
-			$scope.stop = response.data;
-            if($localStorage.favourite != undefined){
-                if($localStorage.favourite.indexOf($scope.stop.stopid) > -1){
-                    $scope.isFavourite = true;
+            if (response.data.errorcode === undefined) {
+                $scope.stop = response.data;
+                if($localStorage.favourite != undefined){
+                    if($localStorage.favourite.indexOf($scope.stop.stopid) > -1){
+                        $scope.isFavourite = true;
+                    }
+                }else{
+                    $localStorage.favourite = [];
                 }
-            }else{
-                $localStorage.favourite = [];
-            }
-			$scope.stops = response.data.results;
-			angular.forEach($scope.stops, function (d) {
-				var currentTime = moment($scope.stop.timestamp, 'DD/MM/YYYY hh:mm:ss').unix() * 1000;
-				var newDate = moment(d.arrivaldatetime, 'DD/MM/YYYY hh:mm:ss').unix() * 1000;
-				var difference = newDate - currentTime;
-				var daysDifference = Math.floor(difference / 1000 / 60 / 60 / 24);
-				difference -= daysDifference * 1000 * 60 * 60 * 24;
-				var hoursDifference = Math.floor(difference / 1000 / 60 / 60);
-				difference -= hoursDifference * 1000 * 60 * 60;
-				var minutesDifference = Math.floor(difference / 1000 / 60);
-				difference -= minutesDifference * 1000 * 60;
-				var secondsDifference = Math.floor(difference / 1000);
-				var daysDifferenceText = "";
-				var hoursDifferenceText = "";
-				var minutesDifferenceText = "";
-				var secondsDifferenceText = "";
-				if(daysDifference > 0){
-					daysDifferenceText = daysDifference + ' day/s ';
-				}
-				if(hoursDifference > 0){
-					hoursDifferenceText = hoursDifference + ' hour ';
-				}
-				if(hoursDifference > 1){
-					hoursDifferenceText = hoursDifference + ' hours ';
-				}
-				if(minutesDifference > 0){
-					minutesDifferenceText = minutesDifference + ' min ';
-				}
-				if(minutesDifference > 1){
-					minutesDifferenceText = minutesDifference + ' mins ';
-				}
-				if(minutesDifference == 0 && secondsDifference > 0){
-					secondsDifferenceText = secondsDifference + ' seconds ';
-				}
-				d.estArrival = daysDifferenceText + hoursDifferenceText + minutesDifferenceText + secondsDifferenceText;
+                $scope.stops = response.data.results;
+                angular.forEach($scope.stops, function (d) {
+                    var currentTime = moment($scope.stop.timestamp, 'DD/MM/YYYY hh:mm:ss').unix() * 1000;
+                    var newDate = moment(d.arrivaldatetime, 'DD/MM/YYYY hh:mm:ss').unix() * 1000;
+                    var difference = newDate - currentTime;
+                    var daysDifference = Math.floor(difference / 1000 / 60 / 60 / 24);
+                    difference -= daysDifference * 1000 * 60 * 60 * 24;
+                    var hoursDifference = Math.floor(difference / 1000 / 60 / 60);
+                    difference -= hoursDifference * 1000 * 60 * 60;
+                    var minutesDifference = Math.floor(difference / 1000 / 60);
+                    difference -= minutesDifference * 1000 * 60;
+                    var secondsDifference = Math.floor(difference / 1000);
+                    var daysDifferenceText = "";
+                    var hoursDifferenceText = "";
+                    var minutesDifferenceText = "";
+                    var secondsDifferenceText = "";
+                    if(daysDifference > 0){
+                        daysDifferenceText = daysDifference + ' day/s ';
+                    }
+                    if(hoursDifference > 0){
+                        hoursDifferenceText = hoursDifference + ' hour ';
+                    }
+                    if(hoursDifference > 1){
+                        hoursDifferenceText = hoursDifference + ' hours ';
+                    }
+                    if(minutesDifference > 0){
+                        minutesDifferenceText = minutesDifference + ' min ';
+                    }
+                    if(minutesDifference > 1){
+                        minutesDifferenceText = minutesDifference + ' mins ';
+                    }
+                    if(minutesDifference == 0 && secondsDifference > 0){
+                        secondsDifferenceText = secondsDifference + ' seconds ';
+                    }
+                    d.estArrival = daysDifferenceText + hoursDifferenceText + minutesDifferenceText + secondsDifferenceText;
 
-			});
+                });
+            } else {
+                $location.path('/');
+                $mdToast.show($mdToast.simple().textContent('Stop not found! lel').position('bottom center'));
+            }
 		});
 	};
 	$scope.checkStop();
