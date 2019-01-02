974cc827e5158a1f79a41fccfd843b5ed2f76d9c
Thomas Behan <thomas.behan@rentalmatix.com>
2019-01-02 23:41:41 +0000

    #16 - updated design - made the design more uniform - updated all the pages to be smoother and include more animations to improve usability

---
+++ platforms/android/app/build/intermediates/assets/release/www/src/AppController.js
@@ -0,0 +1,48 @@
+/**
+ * Main App Controller for the AngularJS Material Starter App
+ * @param UsersDataService
+ * @param $mdSidenav
+ * @constructor
+ */
+function AppController(UsersDataService, $mdSidenav, $scope) {
+  var self = this;
+
+  self.selected     = null;
+  self.users        = [ ];
+  self.selectUser   = selectUser;
+  self.toggleList   = toggleUsersList;
+
+  // Load all registered users
+
+  UsersDataService
+        .loadAllUsers()
+        .then( function( users ) {
+          self.users    = [].concat(users);
+          self.selected = users[0];
+        });
+
+  // *********************************
+  // Internal methods
+  // *********************************
+
+  /**
+   * Hide or Show the 'left' sideNav area
+   */
+  function toggleUsersList() {
+    $mdSidenav('left').toggle();
+  }
+    $scope.sidenavItems = [
+        {"name": "Home", "icon": "home_outlined", "desc": "The main page including search and favourites", "link": "#/"},
+        {"name": "Routes", "icon": "directions_outlined", "desc": "This is for seatching by routes ", "link": "#/routes"},
+        {"name": "Settings", "icon": "settings_outlined", "desc": "A settings/about page", "link": "#/settings"}
+    ];
+  /**
+   * Select the current avatars
+   * @param menuId
+   */
+  function selectUser ( user ) {
+    self.selected = angular.isNumber(user) ? $scope.users[user] : user;
+  }
+}
+
+export default [ 'UsersDataService', '$mdSidenav', '$scope', AppController ];
--- platforms/android/assets/www/src/templates/settings/settings.controller.js
+++
@@ -1,5 +0,0 @@
-export default function settingsController($scope, $http, $location, $mdDialog, $mdToast, $localStorage) {
-    $scope.saveSettings = function(){
-        $mdToast.show($mdToast.simple().textContent('Nothing to save right now').hideDelay(3000));
-    }
-}
