ed31521cc071422f7a3f211702dc51ed2a9b9ae3
Thomas Behan <thomas.behan@rentalmatix.com>
2019-01-04 01:09:29 +0000

    #17 - added stops page

---
+++ www/src/templates/stops/stops.controller.js
@@ -0,0 +1,29 @@
+export default function stopsController($scope, $routeParams, $http, $location, $interval, $rootScope, $localStorage) {
+    $rootScope.loading = true;
+    $scope.url = "https://data.smartdublin.ie/cgi-bin/rtpi/busstopinformation?stopid&format=json";
+    $scope.operatorUrl = "https://data.smartdublin.ie/cgi-bin/rtpi/operatorinformation?format=json";
+    $scope.operators = null;
+    
+    $scope.getStops = function () {
+        $rootScope.loading = true;
+        $http.get($scope.url).then(function (response) {
+            $rootScope.loading = false;
+            $scope.stops = response.data.results;
+            angular.forEach($scope.stops, function(obj){
+                angular.forEach(obj.operators, function(opObj){
+                   opObj.fullname = $scope.operators[$scope.operators.findIndex( operator => operator.operatorreference === opObj.name )].operatorname;
+                    
+                });
+            });
+        });
+    };
+    $scope.getOperatorInformation = function () {
+        $rootScope.loading = true;
+        $http.get($scope.operatorUrl).then(function (response) {
+            $rootScope.loading = false;
+            $scope.operators = response.data.results;
+            $scope.getStops();
+        });
+    };
+    $scope.getOperatorInformation();
+};
