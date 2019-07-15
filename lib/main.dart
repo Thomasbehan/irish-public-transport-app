5a12cc257f9a3dcacb7dec91253b07648cd1bf0c
Thomas Behan <thomas.behan@rentalmatix.com>
2019-07-15 23:30:44 +0100

    #31 - Simplification of the crash reporting

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,17 @@
 import 'package:flutter/material.dart';
 import 'pages/Home.dart';
-import 'package:flutter_crashlytics/flutter_crashlytics.dart';
-import 'dart:async';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
-void main() async {
-  bool isInDebugMode = false;
+void main() {
+  Crashlytics.instance.enableInDevMode = true;
 
+  // Pass all uncaught errors to Crashlytics.
   FlutterError.onError = (FlutterErrorDetails details) {
-    if (isInDebugMode) {
-      // In development mode simply print to console.
-      FlutterError.dumpErrorToConsole(details);
-    } else {
-      // In production mode report to the application zone to report to
-      // Crashlytics.
-      Zone.current.handleUncaughtError(details.exception, details.stack);
-    }
+    Crashlytics.instance.onError(details);
   };
 
-  await FlutterCrashlytics().initialize();
-
-  runZoned<Future<Null>>(() async {
-    runApp(MaterialApp(
-      title: 'IPT ALPHA',
-      home: new HomePage(),
-    ));
-  }, onError: (error, stackTrace) async {
-    // Whenever an error occurs, call the `reportCrash` function. This will send
-    // Dart errors to our dev console or Crashlytics depending on the environment.
-    await FlutterCrashlytics().reportCrash(error, stackTrace, forceCrash: false);
-  });
-
-
+  runApp(MaterialApp(
+    title: 'IPT ALPHA',
+    home: new HomePage(),
+  ));
 }
