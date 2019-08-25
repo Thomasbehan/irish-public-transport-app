c476a85f7f9f87fe0d120b787f39f5d921032b48
Thomas Behan <thomasbehan4@gmal.com>
2019-08-25 20:04:04 +0100

    #28 - add autorefresh and settings page

--- lib/classes/HomeData.dart
+++ lib/classes/HomeData.dart
@@ -15,5 +15,5 @@ final sampleItems = <IntroItem>[
   new IntroItem(title: 'View Your Favourites', category: 'FAVOURITES', imageUrl: 'assets/fav.jpg',),
   new IntroItem(title: 'View All Available Routes', category: 'ROUTES', imageUrl: 'assets/1.jpg',),
   new IntroItem(title: 'View All Stops & Terminals', category: 'STOPS', imageUrl: 'assets/2.jpg',),
-  new IntroItem(title: 'Settings Available Soon', category: 'SETTINGS', imageUrl: 'assets/3.jpg',),
+  new IntroItem(title: 'Customise Your Experience', category: 'SETTINGS', imageUrl: 'assets/settings.jpg',),
 ];
--- lib/pages/Favourites.dart
+++ lib/pages/Favourites.dart
@@ -3,11 +3,13 @@
  */
 
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:irish_public_transport_app/elements/sidemenu.dart';
 import 'package:irish_public_transport_app/elements/topBar.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'Realtime.dart';
 import 'package:toast/toast.dart';
+import 'package:irish_public_transport_app/pages/Home.dart';
+
+import 'Realtime.dart';
 
 class Favourites extends StatefulWidget {
   Favourites({Key key, this.title}) : super(key: key);
@@ -26,8 +28,12 @@ class _FavouritesState extends State<Favourites> {
 
   final storage = new FlutterSecureStorage();
   Map<String, String> allValues;
+
   Future loadFavourites() async {
     Map<String, String> allValues = await storage.readAll();
+    if (allValues.isNotEmpty) {
+      allValues.removeWhere((k, v) => k.contains("Setting_"));
+    }
     setState(() {
       stops = allValues;
     });
@@ -45,8 +51,10 @@ class _FavouritesState extends State<Favourites> {
       controller.dispose();
       super.dispose();
     }
+
     loadFavourites();
   }
+
   void _goToRealtime(stopId) {
     Navigator.push(
       context,
@@ -56,79 +64,88 @@ class _FavouritesState extends State<Favourites> {
 
   Future removeFavourite(stop) async {
     await storage.delete(key: stop.toString());
-    Toast.show(stop.toString() + " Removed From Your Favourites", context, duration: Toast.LENGTH_LONG, gravity:  Toast.BOTTOM);
+    Toast.show(stop.toString() + " Removed From Your Favourites", context,
+        duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
   }
 
   @override
   Widget build(BuildContext context) {
-
-
     final topAppBar = topBar.get(context, 'Your Favourites');
 
-    return Scaffold(
-        appBar: topAppBar,
-        drawer: sidemenu.getMenu(context),
-        body: new Material(
-            child: new Column(children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: new TextField(
-                  decoration: new InputDecoration(
-                      labelText: "Search Your Favourites... "),
-                  controller: controller,
-                ),
+    return new WillPopScope(
+      child: Scaffold(
+          appBar: topAppBar,
+          drawer: sidemenu.getMenu(context),
+          body: new Material(
+              child: new Column(children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: new TextField(
+                decoration: new InputDecoration(
+                    labelText: "Search Your Favourites... "),
+                controller: controller,
               ),
-              new Expanded(
-                child: new ListView.builder(
-                  itemCount: stops == null ? 0 : stops.length,
-                  itemBuilder: (BuildContext context, index) {
-                    String i = stops.keys.elementAt(index);
-                    return filter == null || filter == ""
-                        ? Dismissible(
-                        onDismissed: (direction){
-                          removeFavourite(stops[i]);
+            ),
+            new Expanded(
+              child: new ListView.builder(
+                itemCount: stops == null ? 0 : stops.length,
+                itemBuilder: (BuildContext context, index) {
+                  String i = stops.keys.elementAt(index);
+                  return filter == null || filter == ""
+                      ? Dismissible(
+                          onDismissed: (direction) {
+                            removeFavourite(stops[i]);
                           },
-                        background: Container(color: Colors.red),
-                        key: Key(stops[i]),
-                      child: new InkWell(
-                        onTap: () {
-                          this._goToRealtime(stops[i]);
-                        },
-                        child: new Card(
-                            child: new ListTile(
-                                title: new Text(stops[i]),
-                                subtitle: new Text("Touch to view realtime data"),
-                                leading: new CircleAvatar(
-                                    child: new Icon(
-                                      Icons.nature_people,
-                                      color: Colors.white,
-                                      size: 30.0,
-                                    ))))))
-                        : stops[i].toLowerCase().contains(filter)
-                        ? Dismissible(
-                        onDismissed: (direction){
-                          removeFavourite(stops[i]);
-                        },
-                        background: Container(color: Colors.red),
-                        key: Key(stops[i]),
-                        child: new InkWell(
-                        onTap: () {
-                          this._goToRealtime(stops[i]);
-                        },
-                        child: new Card(
-                            child: new ListTile(
-                                title: new Text(stops[i]),
-                                subtitle: new Text("Touch to view realtime data"),
-                                leading: new CircleAvatar(
-                                    child: new Icon(
-                                      Icons.nature_people,
-                                      color: Colors.white,
-                                      size: 30.0,
-                                    ))))))
-                        : new Container();
-                  },
-                ),
+                          background: Container(color: Colors.red),
+                          key: Key(stops[i]),
+                          child: new InkWell(
+                              onTap: () {
+                                this._goToRealtime(stops[i]);
+                              },
+                              child: new Card(
+                                  child: new ListTile(
+                                      title: new Text(stops[i]),
+                                      subtitle: new Text(
+                                          "Touch to view realtime data"),
+                                      leading: new CircleAvatar(
+                                          child: new Icon(
+                                        Icons.nature_people,
+                                        color: Colors.white,
+                                        size: 30.0,
+                                      ))))))
+                      : stops[i].toLowerCase().contains(filter)
+                          ? Dismissible(
+                              onDismissed: (direction) {
+                                removeFavourite(stops[i]);
+                              },
+                              background: Container(color: Colors.red),
+                              key: Key(stops[i]),
+                              child: new InkWell(
+                                  onTap: () {
+                                    this._goToRealtime(stops[i]);
+                                  },
+                                  child: new Card(
+                                      child: new ListTile(
+                                          title: new Text(stops[i]),
+                                          subtitle: new Text(
+                                              "Touch to view realtime data"),
+                                          leading: new CircleAvatar(
+                                              child: new Icon(
+                                            Icons.nature_people,
+                                            color: Colors.white,
+                                            size: 30.0,
+                                          ))))))
+                          : new Container();
+                },
               ),
-            ])));
+            ),
+          ]))),
+      onWillPop: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      },
+    );
   }
 }
